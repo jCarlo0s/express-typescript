d9ae41a6af3846828ef4041ef4c5867b0976b2df
jCarlo0s <juancarlos.andrade@amk-technologies.com>
2018-07-20 13:41:52 -0500

    added docker configuration

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from 'express';
 
+/**
+ * @interface User
+ */
 interface User {
-    name: string;
+    firstName: string;
+    lastName: string;
+    address: string;
     age: number;
 }
 
+/**
+ * @class UserController
+ */
 export class UserController {
     public getUsers(req: Request, res: Response) {
         const user: User = {
-            name: 'juan',
+            firstName: 'juan carlos',
+            lastName: 'Andrade',
+            address: 'Conocida',
             age: 30
         };
         res.status(200).send(user);
-    } 
+    }
 }
